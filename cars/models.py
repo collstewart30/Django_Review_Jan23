0261b1b133b5a9253929c4cd30193d2de630fcdc
Colleen Stewart <collstewart30@gmail.com>
2023-01-30 16:52:58 -0500

    Created Car Model for Cars App. Registered cars app and REST framework in project settings installed apps section

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    
