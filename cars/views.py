84b05888023b083756be812cbb4b05a22141a7e9
Colleen Stewart <collstewart30@gmail.com>
2023-01-30 17:32:03 -0500

    Registered Car model with admin site

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Car
+from .serializers import CarSerialzer
 
 @api_view(['GET'])
 def cars_list(request):
-    return Response('ok')
+    cars = Car.objects.all()
+
+    serializer = CarSerialzer(cars, many=True)
+
+    print(serializer.data)
+
+    return Response(serializer.data)
