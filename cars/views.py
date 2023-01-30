8d8c7d830e75a32519546160ea2b5c0c684ed254
Colleen Stewart <collstewart30@gmail.com>
2023-01-30 18:10:16 -0500

    Completed delete functionality for car view file

--- cars/views.py
+++ cars/views.py
@@ -19,7 +19,7 @@ def cars_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def car_detail(request, pk):
     car = get_object_or_404(Car, pk=pk)
     if request.method == 'GET':
@@ -30,3 +30,6 @@ def car_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
